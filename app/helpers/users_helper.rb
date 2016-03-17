d843db8be549a5c5faca32c6e9ed8cc91dd30ef5
jeesong <jeewsong@gmail.com>
2016-03-16 21:04:17 -0700

    restrict access & updates  to logged in users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
 
   # Returns gravatar for the user
-  def gravator_for(user, options = { size: 80 })
+  def gravatar_for(user, options = { size: 80 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
