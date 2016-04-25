a6bc2e2278837db872c4f8119397152cb7e4b375
jeesong <jeewsong@gmail.com>
2016-04-25 16:06:31 -0700

    Set up acquire for home page, edit hooks controller for notes from leads

--- app/controllers/hooks_controller.rb
+++ app/controllers/hooks_controller.rb
@@ -5,11 +5,12 @@ class HooksController < ApplicationController
     request.body.rewind
     if request.body.present?
       notification = JSON.parse(request.body.read)
+      notification["data"]["item"]["user"]["name"].empty?
       webhook_note = WebhookNote.new(
         link: notification["data"]["item"]["links"]["conversation_web"],
         body: notification["data"]["item"]["conversation_parts"]["conversation_parts"][0]["body"],
-        name: notification["data"]["item"]["user"]["name"],
-        email: notification["data"]["item"]["user"]["email"],
+        name: notification["data"]["item"]["user"]["name"].empty? ? "Lead" : notification["data"]["item"]["user"]["name"],
+        email: notification["data"]["item"]["user"]["email"].empty? ? "No email" : notification["data"]["item"]["user"]["email"],
         requested_at: notification["data"]["item"]["conversation_parts"]["conversation_parts"][0]["created_at"]
       )
       webhook_note.save!
